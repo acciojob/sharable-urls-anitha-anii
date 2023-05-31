f6b1e98abb39a01188fe2afc368cc8f767cab6a2
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-31 14:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+ 
+  let sbutton = document.getElementById('button');
+  let nvalue = document.getElementById('name');
+  let yvalue = document.getElementById('year');
+   let h3el = document.getElementById('url');
+
+sbutton.addEventListener('click',()=>{
+	let valuename =nvalue.value; 
+	let valueyear =yvalue.value; 
+	let valueh3 = h3el.innerText;
+	 h3el.innerText = `${valueh3}?name=${valuename}&year=${valueyear}`;
+
+	
+});
